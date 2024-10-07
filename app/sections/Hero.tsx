42ce5a61a57e93cd6d34d8868ff79d267d957b13
MalikUmair5 <malikumair.dev@gmail.com>
2024-10-07 13:21:27 -0700

    Working on Responsivness of Hero Section

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className=" flex items-center xs:flex-wrap md:flex-nowrap">
         <div className="">
           <h1 className=" font-bold text-[60px] leading-normal tracking-tighter">Audience of your kind</h1>
-          <p className="w-[580px] text-[18px] tracking-[0.24px]">
+          <p className="w-[580px] text-[18px] tracking-[0.24px]"> 
             We аre аn аgenсy thаt believe in the роwer оf digitаl tо trаnsfоrm
             yоur business. Whether it be а new website, mаrketing саmраign оr
             рrоduсtivity imрrоving sоftwаre, digitаl саn trаnsfоrm аnd grоw yоur
@@ -12,7 +12,7 @@ const Hero = () => {
           </p>
         </div>
         <div className="w-[0] h-[250px] sm:hidden xs:hidden"></div>
-        <div className=" w-[600px] lg:h-[500px] z-[-1] bg-[#2F80ED] lg:absolute top-0 right-0 lg:flex items-center justify-center text-white lg:rounded-bl-[200px] sm:flex sm:rounded-bl-[0px] md:rounded-md md:h-auto md:p-5">
+        <div className=" w-[600px] lg:h-[500px] z-[-1] bg-[#2F80ED] lg:absolute top-0 right-0 lg:flex items-center justify-center text-white lg:rounded-bl-[200px] sm:flex sm:rounded-bl-[0px] md:rounded-md md:h-auto md:p-5 lg:rounded-none">
           <ul className=" text-[19px] font-[600px] space-y-1 tracking-tighter">
             <li className=" font-thin">We Provide:</li>
             <li>App Devolopment</li>
